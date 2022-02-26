482081ae3d8c8c45682b7182135be8f3a6040571
williams861 <85369609+williams861@users.noreply.github.com>
2022-02-25 18:03:12 -0800

    Camera Controls

--- game_controls.py
+++ game_controls.py
@@ -229,13 +229,51 @@ def finger_tracking():
     import multithreaded_webcam as mw
     import mediapipe as mp
 
-    # Sleep for 2 seconds to let camera initialize properly
-    time.sleep(2)
-    # Start video capture
-    vs = mw.WebcamVideoStream().start()
-    #print("Webcam is ON")
-    # vs.stream.release()
+    cap = cv2.VideoCapture(0)
+    TODO: change this and am
+    mpHands = mp.solutions.hands
+    hands = mpHands.Hands(static_image_mode=False,
+                      max_num_hands=2,
+                      min_detection_confidence=0.5,
+                      min_tracking_confidence=0.5)
+    mpDraw = mp.solutions.drawing_utils
 
+    pTime = 0
+    cTime = 0
+
+    while True:
+        success, img = cap.read()
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = hands.process(imgRGB)
+    #print(results.multi_hand_landmarks)
+
+        if results.multi_hand_landmarks:
+            for handLms in results.multi_hand_landmarks:
+                for id, lm in enumerate(handLms.landmark):
+                    print(id,lm)
+                    h, w, c = img.shape
+                    cx, cy = int(lm.x *w), int(lm.y*h)
+                    #if id ==0:
+                    cv2.circle(img, (cx,cy), 3, (255,0,255), cv2.FILLED)
+
+                mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+                Thumb: landmarkList[4][1] < landmarkList[3][1]
+                Index finger: landmarkList[8][2] < landmarkList[6][2]
+                Middle finger: landmarkList[12][2] < landmarkList[10][2]
+                Ring finger: landmarkList[16][2] < landmarkList[14][2]
+                Little finger: landmarkList[20][2] < landmarkList[18][2]
+
+
+
+        cTime = time.time()
+        fps = 1/(cTime-pTime)
+        pTime = cTime
+
+        cv2.putText(img,str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
     # put your code here
 
 
