414006ecfd060e800136d922ef811a053ffebcc0
Blaine Coleman <bcoleman@sandiego.edu>
2022-03-03 21:09:14 -0800

    Final changes, reflection added

--- game_controls.py
+++ game_controls.py
@@ -1,7 +1,13 @@
-from glob import glob
-from turtle import reset, up
-from unittest import result
+'''
+    File name: game_controls.py
+    Authors: Naveen Kumar Rajesh, Blaine Coleman, Cameron Williams
+    Date created: 02/08/2022
+    Date last modified: 03/03/2022
+    Python Version: 3.9.4
+'''
+# --------------------------------- #
 import pyautogui
+# --------------------------------- #
 
 last_position = (None, None)
 last_dir = ''
@@ -32,9 +38,11 @@ def trackpad_mouse():
     Control the game by moving the mouse/finger on trackpad left, right, up, or down. 
     '''
 
+    # --------------------------------- #
     from pynput import mouse
     from pynput.mouse import Controller  # Added for reading pointer
     mouse_pos = Controller()
+    # --------------------------------- #
 
     def on_move(x, y):
         global last_position
@@ -55,25 +63,25 @@ def trackpad_mouse():
 
             if (x_pos_dis > num_threshold[0]):
                 if (x_pos_dis > 0 and last_dir != "left"):
-                    # pyautogui.press('left')
-                    print("left\n")
+                    pyautogui.press('left')
+                    # print("left\n") # Testing
                     last_dir = "left"
                     last_position = (x, y)
                 else:
-                    # pyautogui.press('right')
-                    print("right\n")
+                    pyautogui.press('right')
+                    # print("right\n") # Testing
                     last_dir = "right"
                     last_position = (x, y)
 
             elif (y_pos_dis > num_threshold[1]):
                 if (y_pos_dis > 0 and last_dir != "up"):
-                    # pyautogui.press('up')
-                    print("up\n")
+                    pyautogui.press('up')
+                    # print("up\n") # Testing
                     last_dir = "up"
                     last_position = (x, y)
                 else:
-                    # pyautogui.press('down')
-                    print("down\n")
+                    pyautogui.press('down')
+                    # print("down\n") # Testing
                     last_dir = "down"
                     last_position = (x, y)
 
@@ -94,8 +102,8 @@ def color_tracker():
     # --------------------------------- #
 
     # You need to define HSV colour range MAKE CHANGE HERE
-    colorLower = (29, 86, 6)  # green 
-    colorUpper = (64, 255, 255)  #darkish green
+    colorLower = (29, 86, 6)  # green
+    colorUpper = (64, 255, 255)  # darkish green
 
     # Set the limit for the number of frames to store and the number that have seen direction change
     buffer = 20
@@ -132,7 +140,8 @@ def color_tracker():
         green_mask = cv2.dilate(green_mask, None, iterations=2)
 
         # Finding the contours; Function will return a tuple or two items. We will only need the first:
-        contours,ext = cv2.findContours(green_mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        contours, ext = cv2.findContours(
+            green_mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         # Track the center
         center = None
@@ -156,9 +165,9 @@ def color_tracker():
             x_diff = pts[9][0] - pts[0][0]  # 1st frame
             y_diff = pts[9][1] - pts[0][1]  # 2nd frame
             (dX, dY) = (x_diff, y_diff)
-         
+
         if (abs(dX) > num_threshold[0] or abs(dY) > num_threshold[1]):
-            #setting direction
+            # setting direction
             if(abs(dX) > abs(dY)):
                 if ((dX < 0) and (last_dir != "right")):
                     direction = "right"
@@ -171,25 +180,26 @@ def color_tracker():
                 elif ((dY > 0) and (last_dir != "up")):
                     direction = "up"
 
-            #Show Direction
-            cv2.putText(frame, direction, (20,40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),3)
+            # Show Direction
+            cv2.putText(frame, direction, (20, 40),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
 
             if (direction == "left"):
                 last_dir = "left"
                 pyautogui.press("left")
-                print("left")
+                # print("left") # Testing
             elif (direction == "right"):
                 last_dir = "right"
                 pyautogui.press("right")
-                print("right")
+                # print("right") # Testing
             elif (direction == "up"):
                 last_dir = "up"
                 pyautogui.press("up")
-                print("up")
+                # print("up") # Testing
             elif (direction == "down"):
                 last_dir = "down"
                 pyautogui.press("down")
-                print("down")
+                # print("down") # Testing
 
         # Show the frame on screen
         cv2.imshow('Game Control Window', frame)
@@ -200,111 +210,118 @@ def color_tracker():
 
 
 def finger_tracking():
+    # --------------------------------- #
     import cv2
     import imutils
     import numpy as np
     import time
     import multithreaded_webcam as mw
     import mediapipe as mp
+    # --------------------------------- #
 
     # Sleep for 2 seconds to let camera initialize properly
     time.sleep(2)
 
     video_stream = mw.WebcamVideoStream().start()
-    
-    #hand detection
+
+    # hand detection
     one_hand = mp.solutions.hands
 
-    #getting basic info
-    hands = one_hand.Hands(static_image_mode=False,max_num_hands=2,min_detection_confidence=0.5,min_tracking_confidence=0.5)
+    # getting basic info
+    hands = one_hand.Hands(static_image_mode=False, max_num_hands=2,
+                           min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
-    #draw
+    # draw
     mpDraw = mp.solutions.drawing_utils
 
-    #track last direction 
+    # track last direction
     global last_dir
 
     while True:
 
-        FingerCount = 0
-        landmarkList = []
+        finger_count = 0
+        landmark_list = []
 
         # Reading the frame from the video stream
         frame = video_stream.read()
         # Using that frame, you will flip it
         frame = cv2.flip(frame, 1)
-        # You will then resize the frame
+        # Resize the frame
         frame = imutils.resize(frame, width=600)
-        #cnvert to RGB
+        # Convert to RGB
         rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         # print(results.multi_hand_landmarks)
 
-        #get results from processing the image for hands
+        # Results from processing the image for hands
         results = hands.process(rgb)
 
         landmarks = results.multi_hand_landmarks
 
-        #for loop to go though all of the "multi_hand_landmarks" 
+        # Loop though all of the "multi_hand_landmarks"
         if landmarks != None:
             for handLms in results.multi_hand_landmarks:
                 for id, lm in enumerate(handLms.landmark):
-                    h, w = rgb.shape[0:2]         #getting dimensions 
+                    h, w = rgb.shape[0:2]  # getting dimensions
                     cx, cy = int(lm.x * w), int(lm.y*h)
-                    cv2.circle(rgb, (cx, cy), 3, (255, 0, 255), cv2.FILLED) #see on frame
-                    landmarkList.append((id,cx,cy))  # append values to landmark list 
-                mpDraw.draw_landmarks(frame, handLms, one_hand.HAND_CONNECTIONS)   #draw
-            
-            if landmarkList != None:
-                thumb = landmarkList[4][1] < landmarkList[3][1]
-                index = landmarkList[8][2] < landmarkList[6][2]
-                middle = landmarkList[12][2] < landmarkList[10][2]
-                ring = landmarkList[16][2] < landmarkList[14][2]
-                little = landmarkList[20][2] < landmarkList[18][2]
+                    cv2.circle(rgb, (cx, cy), 3, (255, 0, 255),
+                               cv2.FILLED)  # see on frame
+                    landmark_list.append((id, cx, cy))
+                mpDraw.draw_landmarks(
+                    frame, handLms, one_hand.HAND_CONNECTIONS)  # draw
 
-            if thumb == True:        #keeping track of the number of fingers held up
-                FingerCount +=1
+            if landmark_list != None:
+                thumb = landmark_list[4][1] < landmark_list[3][1]
+                index = landmark_list[8][2] < landmark_list[6][2]
+                middle = landmark_list[12][2] < landmark_list[10][2]
+                ring = landmark_list[16][2] < landmark_list[14][2]
+                little = landmark_list[20][2] < landmark_list[18][2]
+
+            if thumb == True:  # keeping track of the number of fingers held up
+                finger_count += 1
             if index == True:
-                FingerCount +=1
+                finger_count += 1
             if middle == True:
-                FingerCount +=1
+                finger_count += 1
             if ring == True:
-                FingerCount +=1
+                finger_count += 1
             if little == True:
-                FingerCount +=1
-            
-            if FingerCount != 0:     #mapping controls to appropriate finger value 
-                if FingerCount == 1 and last_dir != "up":
+                finger_count += 1
+
+            if finger_count != 0:  # Mapping controls for finger value
+                if finger_count == 1 and last_dir != "up":
                     pyautogui.press("up")
-                    print("up")
-                    FingerCount = 1
+                    # print("up") # Testing
+                    finger_count = 1
                     last_dir = "up"
-                if FingerCount == 2 and last_dir != "down":
+                if finger_count == 2 and last_dir != "down":
                     pyautogui.press("down")
-                    print("down")
-                    FingerCount = 2
+                    # print("down") # Testing
+                    finger_count = 2
                     last_dir = "down"
-                if FingerCount == 3 and last_dir != "left":
+                if finger_count == 3 and last_dir != "left":
                     pyautogui.press("left")
-                    print("left")
-                    FingerCount = 3
+                    # print("left") # Testing
+                    finger_count = 3
                     last_dir = "left"
-                if FingerCount == 4 and last_dir != "right":
+                if finger_count == 4 and last_dir != "right":
                     pyautogui.press("right")
-                    print("right")
-                    FingerCount = 4
+                    # print("right") # Testing
+                    finger_count = 4
                     last_dir = "right"
-                
-        #display finger counts on screen
-        cv2.putText(frame, str(int(FingerCount)), (10, 70),cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+
+        # display finger counts on screen
+        cv2.putText(frame, str(int(finger_count)), (10, 70),
+                    cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
         cv2.imshow("Image", frame)
         cv2.waitKey(1)
-    
+
 # End finger_tracking()----------------------------------------------------- #
 
 
 def unique_control():
+    # --------------------------------- #
     import speech_recognition as sr
-    # need to install speechreconition
+    # --------------------------------- #
 
     r = sr.Recognizer()
 
